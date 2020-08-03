57480987f79518de8eba628d451a2cebd9950dc1
Daehyun <dhc4192@gmail.com>
2020-08-03 19:49:39 -0400

    horizontal scroll functionality complete

--- client/src/components/HomePageItems/HomePageItems.jsx
+++ client/src/components/HomePageItems/HomePageItems.jsx
@@ -1,37 +1,53 @@
 import React, { Component } from "react";
 import ScrollMenu from "react-horizontal-scrolling-menu";
-import SampleWine from '../Assets/sample-wine.png'
 
 import "./HomePageItems.css";
 
-const list = [
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
-  { name: <img src={SampleWine} alt=""/> },
+const save = [
+  { name: "save1" },
+  { name: "save2" },
+  { name: "save3" },
+  { name: "save4" },
+  { name: "save5" },
+  { name: "save6" },
+  { name: "save7" },
+  { name: "save8" },
+  { name: "save9" },
+  { name: "save10" },
+  { name: "save11" },
+  { name: "save12" },
+  { name: "save13" },
+];
+const best = [
+  { name: "best1" },
+  { name: "best2" },
+  { name: "best3" },
+  { name: "best4" },
+  { name: "best5" },
+  { name: "best6" },
+  { name: "best7" },
+  { name: "best8" },
+  { name: "best9" },
+  { name: "best10" },
+  { name: "best11" },
+  { name: "best12" },
+  { name: "best13" },
 ];
 
-// One item component
-// selected prop will be passed
 const MenuItem = ({ text }) => {
-  return <div className='menu-item'>{text}</div>;
+  return <div className="menu-item">{text}</div>;
 };
 
-// All items component
-// Important! add unique key
-export const Menu = (list) =>
-  list.map((el) => {
-    const { name } = el;
-
-    return <MenuItem text={name} key={name}/>;
+export const SaveMenu = (save) =>
+  save.map((item) => {
+    const { name } = item;
+    return <MenuItem text={name} key={name} />;
+  });
+  
+export const BestMenu = (save) =>
+  save.map((item) => {
+    const { name } = item;
+    return <MenuItem text={name} key={name} />;
   });
 
 const Arrow = ({ text, className }) => {
@@ -44,19 +60,31 @@ const ArrowRight = Arrow({ text: ">", className: "arrow-next" });
 export default class HomePageItems extends Component {
   constructor(props) {
     super(props);
-    this.menuItems = Menu(list);
+    this.saveItems = SaveMenu(save);
+    this.bestItems = BestMenu(best);
   }
 
   render() {
-    // Create menu from items
-    const menu = this.menuItems;
+    const save = this.saveItems;
+    const best = this.bestItems;
     return (
       <div>
-        <ScrollMenu
-          data={menu}
-          arrowLeft={ArrowLeft}
-          arrowRight={ArrowRight}
-        />
+        <div className='save-container'>
+          <p className='save-text'>SAVE MORE</p>
+          <ScrollMenu
+            data={save}
+            arrowLeft={ArrowLeft}
+            arrowRight={ArrowRight}
+          />
+        </div>
+        <div className='best-container'>
+          <p className='best-text'>TOP SELLING WINES</p>
+          <ScrollMenu
+            data={best}
+            arrowLeft={ArrowLeft}
+            arrowRight={ArrowRight}
+          />
+        </div>
       </div>
     );
   }
