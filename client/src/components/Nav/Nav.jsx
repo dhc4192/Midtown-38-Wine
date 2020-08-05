d9d1bc786e2c53bcf2dae224d49f20468ef96fae
jp <y.polanco12@gmail.com>
2020-08-05 13:12:49 -0400

    updates to nav bar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 import Logo from "../Assets/38WinesStore.jpeg";
 import AccountIcon from "../Assets/account-icon.png";
 import CartIcon from "../Assets/cart-icon.png";
-import {Link} from 'react-router-dom'
-import './Nav.css'
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export default class Nav extends Component {
   render() {
     return (
-      <div className='main-nav-container'>
-
-
-        <div className='nav1-container'>
-          <img className='logo-img' src={Logo} alt="Midtown 38 Wines logo" />
-          <h1>NYC <span className="free">FREE</span> 1 HOUR DELIVERY</h1>
+      <div className="main-nav-container">
+        <div className="nav1-container">
+          <img className="logo-img" src={Logo} alt="Midtown 38 Wines logo" />
+          <h1>
+            NYC <span className="free">FREE</span> 1 HOUR DELIVERY
+          </h1>
           <div>
             <img src={AccountIcon} alt="My account icon" />
             <p>MY ACCOUNT</p>
@@ -24,28 +24,38 @@ export default class Nav extends Component {
           </div>
         </div>
 
-
-        <div className='nav2-container'>
-          <ul className='nav-selectors'>
+        <div className="nav2-container">
+          <div className="nav-selectors">
+            <div class="dropdown">
+              <button class="dropbtn">WINES</button>
+              <div class="dropdown-content">
+                <Link>
+                  <li href="#">Link 1</li>
+                </Link>
+                <Link>
+                  <li href="#">Link 2</li>
+                </Link>
+                <Link>
+                  <li href="#">Link 3</li>
+                </Link>
+              </div>
+            </div>
             <Link>
-              <li className='nav-list'>WINES</li>
+              <li className="nav-list">SPIRITS</li>
             </Link>
             <Link>
-              <li className='nav-list'>SPIRITS</li>
+              <li className="nav-list">SAKE</li>
             </Link>
             <Link>
-              <li className='nav-list'>SAKE</li>
+              <li className="nav-list">KOSHER</li>
             </Link>
             <Link>
-            <li className='nav-list'>KOSHER</li>
+              <li className="nav-list">NEWS</li>
             </Link>
             <Link>
-            <li className='nav-list'>NEWS</li>
+              <li className="nav-list">CONTACT US</li>
             </Link>
-            <Link>
-            <li className='nav-list'>CONTACT US</li>
-            </Link>
-          </ul>
+          </div>
         </div>
       </div>
     );
